aa0b9ad78da844384a74b7a10fec810ee72c6301
Cupprum <branisa.samuel@windowslive.com>
2018-04-04 20:02:53 +0200

    uspesne sa prihlasuje na fb

--- app.py
+++ app.py
@@ -2,7 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import os
 
 
 driver = webdriver.Firefox()
 driver.get("https://www.facebook.com/")
+
+my_fb_mail = os.environ["FB_MAIL"]
+my_fb_pass = os.environ["FB_PASS"]
+
+mail = driver.find_element_by_name("email")
+mail.clear()
+mail.send_keys(my_fb_mail)
+
+password = driver.find_element_by_name("pass")
+password.clear()
+password.send_keys(my_fb_pass)
+password.send_keys(Keys.RETURN)
+
+time.sleep(60)
+
+txt = open("www.html", "w")
+html_source = driver.page_source
+txt.write(html_source)
