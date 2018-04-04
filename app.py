6976310bc15a5cfe33b00e39b1c85c0e65fedf08
Cupprum <branisa.samuel@windowslive.com>
2018-04-04 22:38:30 +0200

    loaduje fb stranku aku nakazenm, problemy s javascriptom

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 
 driver = webdriver.Firefox()
-driver.get("https://www.facebook.com/")
+driver.get("https://www.facebook.com")
 
 my_fb_mail = os.environ["FB_MAIL"]
 my_fb_pass = os.environ["FB_PASS"]
@@ -20,8 +20,18 @@ password.clear()
 password.send_keys(my_fb_pass)
 password.send_keys(Keys.RETURN)
 
-time.sleep(60)
+time.sleep(8)
 
-txt = open("www.html", "w")
-html_source = driver.page_source
-txt.write(html_source)
+webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+driver.get("https://www.facebook.com/SOMERANDOMPERSON")
+
+time.sleep(8)
+
+webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+login_form = driver.find_element_by_xpath("//div[contains(.,'Hello Justin')]")
+
+actions = ActionChains(driver)
+actions.move_to_element(login_form).perform()
+
