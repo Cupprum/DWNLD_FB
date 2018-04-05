e3bc40a393b66fe473ace9f9bf79c308370fd6e7
Cupprum <branisa.samuel@windowslive.com>
2018-04-05 22:08:55 +0200

    vyhladava pocet opakovani

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import os
+import re
 
 
 driver = webdriver.Firefox()
@@ -20,13 +21,13 @@ password.clear()
 password.send_keys(my_fb_pass)
 password.send_keys(Keys.RETURN)
 
-time.sleep(8)
+time.sleep(5)
 
 webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
 
 driver.get("https://www.facebook.com/b.tallova")
 
-time.sleep(8)
+time.sleep(2)
 
 webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
 
@@ -36,13 +37,10 @@ SCROLL_PAUSE_TIME = 0.5
 last_height = driver.execute_script("return document.body.scrollHeight")
 
 while True:
-    # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    # Wait to load page
     time.sleep(SCROLL_PAUSE_TIME)
 
-    # Calculate new scroll height and compare with last scroll height
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
         break
@@ -52,5 +50,16 @@ obsah = str(driver.page_source)
 
 time.sleep(1)
 
-print(obsah.count("Zobraziť 10 ďalších komentárov"))
-print(obsah.count("odpoved"))
+print(obsah.count("Zobraziť"))
+print(obsah.count("odpoveď"))
+
+print(re.findall(r'Zobraziť \d ďalšie komentáre', obsah))
+print(re.findall(r'Zobraziť \d\d ďalších komentárov', obsah))
+
+
+
+"""
+Zobraziť 1 ďalší komentár
+Zobraziť 3 ďalšie komentáre
+Zobraziť 10 ďalších komentárov
+"""
